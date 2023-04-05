ab80ea41039bbd4990cd258f2de6ecaaefad2966
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2023-04-06 00:29:00 +0530

    yah.. i can done

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -2,30 +2,49 @@ import React, { useState, useEffect } from "react";
 import "./Theme.css";
 import { BsFillMoonFill } from "react-icons/bs";
 import { BsFillSunFill } from "react-icons/bs";
+import { ThemeType } from "./types";
 
 function Theme() {
-  const [theme, setTheme] = useState("light-theme");
-  // const [themeImg, setThemeImg] = useState(<BsFillMoonFill />);
-  const toggleThemeHandler = () => {
-    if (theme === "dark-theme") {
-      setTheme("light-theme");
-      // setThemeImg("<BsFillSunFill/>");
-    } else {
-      setTheme("dark-theme");
-      // setThemeImg("<BsFillMoonFill/>");
-    }
-  };
+  // const ThemeType = "dark-theme" | "light-theme";
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const dataTheme = document.body.getAttribute("data-theme");
+  // const [theme, setTheme] =
+  //   useState <
+  //   ThemeType >
+  //   ((): ThemeType => (dataTheme === "light" ? "light" : "dark"));
+
   useEffect(() => {
-    document.body.className = theme;
+    // document.body.className = theme;
+    localStorage.setItem("theme", theme);
+    document.body.setAttribute("data-theme", theme);
   }, [theme]);
-  // useEffect(() => {
-  //   document.body.className = themeImg;
-  // }, [themeImg]);
+
+  const handleSwitchTheme = () => {
+    setTheme(isDark ? "light" : "dark");
+  };
+  const isDark = theme === "dark";
+  // const [theme, setTheme] =
+  //   React.useState <
+  //   ThemeType >
+  //   ((): ThemeType => (dataTheme === "light" ? "light" : "dark"));
+
+  // React.useEffect(() => {
+  //   document.body.setAttribute("data-theme", theme);
+  // }, [theme]);
+
+  // const handleSwitchTheme = () => {
+  //   setTheme(isDark ? "light" : "dark");
+  // };
+
+  // const isDark = theme === "dark";
 
   return (
     <div className="theme-css">
-      <button onClick={toggleThemeHandler}>
+      {/* <button onClick={handleSwitchTheme}>
         <BsFillMoonFill />
+      </button> */}
+      <button className="switch-theme-button" onClick={handleSwitchTheme}>
+        {isDark ? <BsFillSunFill color="white" /> : <BsFillMoonFill />}
       </button>
     </div>
   );
---
+++ src/components/types.js
@@ -0,0 +1,2 @@
+// import { CHANNEL_TYPES } from "../constants";
+export type ThemeType = "dark" | "light";
