9fcf962b2204b098f71edca47dec31ba42617518
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2023-04-15 14:55:25 +0530

    remove ScrollAnimation from mobile

--- src/components/ScrollAnimation.js
+++ src/components/ScrollAnimation.js
@@ -15,38 +15,55 @@ function ScrollAnimation(props) {
     };
   }, []);
 
+  const [isDesktop, setIsDesktop] = React.useState(false);
+
+  React.useEffect(() => {
+    function handleResize() {
+      setIsDesktop(window.innerWidth > 768); // set a minimum width for desktop screens
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // set initial value on mount
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div
-      className="container_scroll"
-      style={{
-        position: "absolute",
-        width: "100%",
-        // height: "50vh",
-        zIndex: "-1",
-        overflow: "hidden",
-        // right: "-300px",
-        // top: "70%",
-      }}
-    >
-      <div
-        className="text"
-        style={{
-          transform: `translateX(${-scrollPosition}px)`,
-          backgroundColor: "none",
-          fontSize: "100px",
-          //   fontFamily
-          right: "-400px",
-          zIndex: "-1",
-          //   top: "60%",
-          //   float: "right",
-          textAlign: "right",
-          //   fontWeight: "700",
-          opacity: "0.3",
-        }}
-      >
-        {props.name}
-      </div>
-    </div>
+    <>
+      {isDesktop && (
+        <div
+          className="container_scroll"
+          style={{
+            position: "absolute",
+            width: "100%",
+            // height: "50vh",
+            zIndex: "-1",
+            overflow: "hidden",
+            // right: "-300px",
+            // top: "70%",
+          }}
+        >
+          <div
+            className="text"
+            style={{
+              transform: `translateX(${-scrollPosition}px)`,
+              backgroundColor: "none",
+              fontSize: "100px",
+              //   fontFamily
+              right: "-400px",
+              zIndex: "-1",
+              //   top: "60%",
+              //   float: "right",
+              textAlign: "right",
+              //   fontWeight: "700",
+              opacity: "0.3",
+            }}
+          >
+            {props.name}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
