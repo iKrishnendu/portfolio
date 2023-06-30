2b319b8536d61ce1c81796562628a895b91d23c1
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2023-07-01 02:07:39 +0530

    footer add sitemap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,32 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   let date = new Date();
   let year = date.getFullYear();
   return (
-    <div className='footer'>
-      
-      <div className='footer--container'>
-        <div className='right'><p>Designed and Developed by Krishnendu Sahoo</p></div>
-        <div className='left'><p>Copyright © {year}</p></div>
+    <div className="footer">
+      <div className="footer--container">
+        <div className="sitemap">
+          <p>SiteMap</p>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/project">Project</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/blog">Blog</Link>
+        </div>
 
+        <div className="right">
+          <p>Designed and Developed by Krishnendu Sahoo</p>
+        </div>
+        <div className="left">
+          <p>Copyright © {year}</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
